2b98f863badd97dc590c5d9d03ad44c4e5e0d5e0
kirankatekk <kirankate117@gmail.com>
2025-11-26 13:55:37 +0530

    Added package

--- backend/server.js
+++ backend/server.js
@@ -42,10 +42,10 @@ async function main() {
 
   const path = require("path");
 
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
   });
 }
 
